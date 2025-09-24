5b42949b9688125b1c01bb9a64ea807d27e1ec95
Telesermark13 <telesermark13@gmail.com>
2025-09-24 10:32:38 +0800

    Fix: add TypeScript Babel preset, update Firebase Gradle config, ensure google-services plugin

--- terralogix-hr/babel.config.js
+++ terralogix-hr/babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    '@babel/preset-typescript'
+  ],
   plugins: [
     ['@babel/plugin-transform-private-methods', { 'loose': true }],
     ['@babel/plugin-transform-class-properties', { 'loose': true }],
