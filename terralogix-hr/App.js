a2ccd49384bf342bbc16b1ff7fe947f38bc5d1cc
Telesermark13 <telesermark13@gmail.com>
2025-09-24 11:07:07 +0800

    sentry

--- terralogix-hr/App.js
+++ terralogix-hr/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { AuthProvider } from "./AuthContext";
 import RootNavigator from "./navigation/RootNavigator";
+import * as Sentry from '@sentry/react-native';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://da3f311cc5a9b246d279874385eb811a@o45100723760167808.ingest.us.sentry.io/4510072376066048',
+  sendDefaultPii: true,
+  enableLogs: true,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+  // spotlight: __DEV__, // Uncomment to enable Spotlight
+});
+
+function App() {
   return (
     <AuthProvider>
       <RootNavigator />
     </AuthProvider>
   );
 }
+
+export default Sentry.wrap(App);
