43eb7f9436edad5499e960bb7824503fab82bb47
Telesermark13 <telesermark13@gmail.com>
2025-09-24 09:37:22 +0800

    Add EAS projectId to app.config.js for secret support

--- terralogix-hr/app.config.js
+++ terralogix-hr/app.config.js
@@ -7,4 +7,11 @@ export default ({ config }) => ({
     package: "com.terralogix.hr",
     googleServicesFile: "./google-services.json"
   },
+  extra: {
+    ...(config.extra || {}),
+    eas: {
+      ...(config.extra?.eas || {}),
+      projectId: "3dfa5117-d674-4045-8ab6-7f3d484ca0df"
+    }
+  }
 });
